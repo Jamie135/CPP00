7c9bff452ec2e9f734f280a5436d27cd6141af6d
Pomprab BUREERAK <pbureera@paul-f4Br5s3.clusters.42paris.fr>
2023-08-04 13:21:05 +0200

    OK

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbureera <pbureera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 13:05:23 by pbureera          #+#    #+#             */
+/*   Updated: 2023/08/04 13:17:19 by pbureera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
+			if (argv[i] != NULL)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
+}
